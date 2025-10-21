1a79e2f4594a3ececb3f91c54965c9bddd20f943
sheymaelali94-cmd <seymaelali090@gmail.com>
2025-10-21 14:14:50 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 IMPORTANT: this must match your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/Banking-Website/',  
+  base: '/Banking-Website/'
 })
